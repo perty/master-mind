1cf9a9ac9e07760a08faadca8da6d487a9bb6d4e
Per Lundholm <per@artcomputer.se>
2022-10-18 15:28:26 +0200

    first commit

---
+++ src/Main.elm
@@ -0,0 +1,72 @@
+module Main exposing (main)
+
+import Array exposing (Array)
+import Browser
+import Html exposing (div, text)
+
+
+type alias Model =
+    { correct : Combination
+    , guesses : List Combination
+    }
+
+
+type Msg
+    = NoOp
+
+
+type alias Combination =
+    Array Peg
+
+
+type Peg
+    = Red
+    | Green
+    | Yellow
+    | Blue
+    | Black
+    | White
+
+
+type CorrectionPeg
+    = BlackCorrection
+    | WhiteCorrection
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+init : Model
+init =
+    { correct = makeCombination Red Green Yellow Blue
+    , guesses = []
+    }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ text "Hello"
+        ]
+
+
+
+-- Gurantee it is always 4 pegs
+
+
+makeCombination : Peg -> Peg -> Peg -> Peg -> Combination
+makeCombination peg1 peg2 peg3 peg4 =
+    Array.fromList [ peg1, peg2, peg3, peg4 ]
